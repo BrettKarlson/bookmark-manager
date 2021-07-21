346d08026f892226fca500be4561309e02ae49ad
Brett Karlson <brettjkarlson@gmail.com>
2021-07-21 22:22:34 +0100

    testing environment set up

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -8,10 +8,17 @@ end
 
 feature 'list of bookmarks' do
   scenario 'view the bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
     visit('/')
     click_button 'Bookmarks'
     expect(page).to have_content 'http://www.makersacademy.com'
-    expect(page).to have_content 'http://www.github.com'
+    expect(page).to have_content 'http://www.destroyallsoftware.com'
+    expect(page).to have_content 'http://www.google.com'
     end
   end
 end
